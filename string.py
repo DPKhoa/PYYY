75bd05a4970f512056a272e11f6f7aa40f5c1f1e
Khoa <anhk312001@gmail.com>
2021-09-17 23:03:26 +0700

    them vao cac dinh dang chuoi

--- string.py
+++ string.py
@@ -1,2 +1,23 @@
-strKhoa = 'Hello Khoa'
+'''Đây là chú thích đầu file py
+còn muốn ghi gì vào thì cứ ghi
+Nó giống như một comment
+'''
+"""strKhoa = ''' HowKteam.com\nFree education\nShare tobe better'''
 print(strKhoa)
+print(type(strKhoa))
+#SOLID
+#STR
+ """
+'''print('\a')
+print('dong1\tdong2')#\n
+print('abcd\be')
+print('xuongdong\ndum cai\nnha')
+print('I\'m beginner')
+print("one thing\"spefical\",that\'s it")
+print'''
+'"Đây là những dòng chú thích  đầu file Về việc import các thư viên, module"'
+import lungtung
+import taolao
+
+def ham_vo_dung():
+	'"Còn đây là docstring cho một function.Đó là hàm vô dụng"'
