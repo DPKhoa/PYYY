e5144b3032e39c943be1e25851edd07f6c47649f
Khoa <anhk312001@gmail.com>
2021-09-19 21:26:48 +0700

    string2

--- string2.py
+++ string2.py
@@ -1,19 +1,19 @@
-#strA = "HowKteam.com"
+strA = "HowKteam.com"
 #strB = "Free education"
 #strB="K"
 #strB = strA[3]
 #strB = strA[-3]
-#strB=strA[len(strA)-1]
+strB=strA[len(strA)-1]
 #strB = strA[1:5]
 #strB = strA[:]
 #strB = strA[:5:-2]
 #strC= strA + "\n" + strB
 #strC = strA*5
 #strC = strB in strA
-#print(strB)
+print(strB)
 #strA = int("69") 
 #strA=int(6.9)
 #strB=str(69) + "5"
-strA="HowKteam"
-strA=strA[None:1] + "0" + strA[2:None]
-print(hash(strA))
+#strA="HowKteam"
+#strA=strA[None:1] + "0" + strA[2:None]
+#print(hash(strA))
