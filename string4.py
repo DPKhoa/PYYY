f5cf4b60a8fa41d99f65247d8c270d285668a3e6
Khoa <anhk312001@gmail.com>
2021-09-20 01:33:27 +0700

    string4

---
+++ string4.py
@@ -0,0 +1,30 @@
+a = "hello Khoa dep trai"
+#chữ đầu tiên viết hoa
+b = a.capitalize()
+#Tất cả các chữ viết hoa
+b = a.upper()#lower tất cả viết thường
+#chữ viết hoa sẽ viết thường và ngược lại  
+b = a.swapcase()
+#print(b)
+a = 'Kteam'
+#canh giữa ,kí tự fillchar và fillchare có đô dài là nên k thể điền --
+b = a.center(30, '*')
+# canh trái rjust , canh trái là ljust
+#print(a)
+#print(b)
+#dùng mã hóa định dạng của kiểu dữ liệu
+a = 'có gì hót'
+b = a.encode()
+#print(a)
+#print(b)
+#lấy danh sách cộng với từng chuỗi
+b=a.join(['1','2','3','4'])
+#print(b)
+#thay thế một chuỗi bằng chuỗi khác
+b=a.replace('ó','Khoa',1)
+#print(b)
+#Trả về chuỗi với phần đầu và phần duôi bỏ đi các kí tự char(mặc định là khoảng trắng)/cắt chữ nằm ở 2 đầu
+a = '   có gì hót'
+b=a.strip()
+#rstrip,lstrip:cắt phải cắt trái
+print(b)
