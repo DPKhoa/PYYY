42a8e43ce8ce4cabb09bb138fd2452cd1bdfe281
Khoa <anhk312001@gmail.com>
2021-09-20 00:48:47 +0700

    string3.py

--- string3.py
+++ string3.py
@@ -1,2 +1,33 @@
-a = 'My name is %s' %('Khoa')
-print(a)
+'''a = 'My name is %s' %('Khoa')
+print(a)'''
+'''s = '%s %s'
+print(s %('1' , '2'))
+s='%.4f' %(3.999)
+print(s)'''
+'''K = 'Kteam'
+result = f'{K} - Free education'
+print(result)'''
+
+'''name='Khoa'
+address='TP.HCM'
+phone='0818830301'
+result = f'Student:{{name}}\nAddress:{{address}}\nPhone:{phone}'
+print(result)'''
+
+"""a = '1: {one}, 2:{two}'.format(one=111 , two=222)
+print(a)"""
+#r = 'How {:*^50} Free education'.format('Khoa')
+#print(r)
+# phần định dạng
+row_1 = '+ {:-<6} + {:-^15} + {:->10} +'.format('', '', '')
+row_2 = '| {:<6} | {:^15} | {:>10} |'.format('ID', 'Ho va ten', 'Noi sinh')
+row_3 = '| {:<6} | {:^15} | {:>10} |'.format('123', 'Kteam', 'TP HCM')
+row_4 = '| {:<6} | {:^15} | {:>10} |'.format('6969', 'Kquiz', 'Da Lat')
+row_5 = '+ {:-<6} + {:-^15} + {:->10} +'.format('', '', '')
+
+# phần xuất kết quả
+print(row_1)
+print(row_2)
+print(row_3)
+print(row_4)
+print(row_5)
