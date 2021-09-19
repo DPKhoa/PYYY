3087aa1e755ebcbda2ac6fc270d54b18bb28e88c
Khoa <anhk312001@gmail.com>
2021-09-20 00:48:31 +0700

    not fixed

--- 1.py
+++ 1.py
@@ -5,3 +5,10 @@ print("hellokhoadeptrai")
 
 #chuỗi có dạng là 1 comment
 print("#đây là comment")
+
+ class SomeThing:
+…         def __repr__(self):
+…                 return ‘Đây là __repr__'
+…         def __str__(self):
+…              return 'Đây là __str__'
+…
