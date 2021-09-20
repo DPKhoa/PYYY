effc15e63b57bfff832a3786149323ef4a1df97c
Khoa <anhk312001@gmail.com>
2021-09-21 01:11:58 +0700

    BT lam them

---
+++ btcodelearn.py
@@ -0,0 +1,30 @@
+'''name = input()
+age = int(input())
+print("In 15 years, age of " +name+ " will be " +str(age+15))'''
+'''a = int(input('a = '))
+b = int(input('b = '))
+print("a % b =" +str(a % b))
+'''
+'''a=float(input('nhập a:'))
+b=float(input('nhập b:'))
+print('tổng: '+ str(format(a+b,".3f")))
+print('hiệu: '+ str(format(a-b,".3f")))
+print('tích: '+ str(format(a*b,".3f")))
+print('thương: '+ str(format(a/b,".3f")))'''
+'''a = int(input())
+b = int(input())
+print("a + b = " +str(a + b))
+print("a - b = " +str(a - b))
+print("a * b = " +str(a * b))
+print("a / b = " +str(format(a / b,".2f")))
+print("a % b = " +str(a % b))'''
+
+'''a = int(input())
+b = int(input())
+c = a
+a = b 
+b = c
+print("after swap a = " + str(a) + ", b = " + str(b))'''
+ 
+r = float(input())
+print("Circumference = "+str(2 *3.14 * r))
