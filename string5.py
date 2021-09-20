84bc2a6d2f0674dc02c98dcefc753dcbe9d57820
Khoa <anhk312001@gmail.com>
2021-09-21 00:02:03 +0700

    string5

---
+++ string5.py
@@ -0,0 +1,22 @@
+a = 'How Kteam free education'
+#b = a.rsplit(' ',2)#rsplit,lsplit
+#cắt chuỗi trước và sau chữ K
+b = a.partition('K')#rpartition
+b = a.rpartition('y')#cắt từ phải
+#tìm số lượng chữ e từ vị trí 0 đến vị trí 10 
+b = a.count('e',0,10)
+b = a.startswith('How',4,9)#vị trí 0 đến vị trí 10
+#b = a.endwith() 
+b = a.find('Kteam')#tìm vị trí có rfind,còn tìm k thấy sẽ quăng ra -1
+#b = a.index('y')# nếu tìm k thấy sẽ quăng ra lỗi 
+b = a.islower()#isupper
+b = a.isdigit()# kiểm tra có phải là số không 
+b = a.isspace( )
+#print(a)
+#print(b)
+s = 'aaaAAaaaooaa neu mot Ngay naO Doaaaaaaa'
+d = s.lstrip('aaaAAaaaooaa')
+k = d.rstrip('aaaaaaa')
+h = k.replace('O','o')
+q = h.title()# viết hoa các chữ cái đầu dòng
+print('aaaAAaaaooaaneu mot Ngay na0 Doaaaaaaa'.strip('aA').lstrip('ao').title().replace('0','o'))
