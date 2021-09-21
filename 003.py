40def47e35ab6dfc753ad81ef9f5dc929dcc388e
Khoa <anhk312001@gmail.com>
2021-09-22 00:21:48 +0700

    003

--- 003.py
+++ 003.py
@@ -1,2 +1,7 @@
-ten = input()
-print("Xin--chao!--Toi--ten--la-- ",ten,sep="--")
+'''ten = input()
+print("Xin--chao!--Toi--ten--la-- ",ten,sep="--")'''#sep dùng để xác đinh khoảng cách
+a = input()
+if a.isalpha() :#kt có phải kiểu kí tự không 
+    print("Xin chao! Ten cua toi la : ", a)
+else :
+    print("Nhap lai dung ten cua ban ")
