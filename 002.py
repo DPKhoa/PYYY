b63c339abd583ae49457e245191d4188e6cae503
Khoa <anhk312001@gmail.com>
2021-09-21 22:35:05 +0700

    002.py

---
+++ 002.py
@@ -0,0 +1,9 @@
+try:
+    print("nhap so a :")
+    soa  = int(input())
+    print("nhap so b :")
+    sob = int(input())
+    tong = soa + sob
+    print("Tong hai so la "+str(tong))
+except:
+    print("Du lieu dau vao k hop le")
