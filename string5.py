a650394ed38f38c48835f1a9ce9b8b770b56a73d
Khoa <anhk312001@gmail.com>
2021-09-26 01:25:21 +0700

    string5

--- string5.py
+++ string5.py
@@ -1,13 +1,19 @@
-a = 'How Kteam free education'
+'''a = 'How Kteam free education'
 #b = a.rsplit(' ',2)#rsplit,lsplit
+print(b)'''
 #cắt chuỗi trước và sau chữ K
-b = a.partition('K')#rpartition
-b = a.rpartition('y')#cắt từ phải
+a = 'How Kteam free education'
+b = a.isspace()
+
+#b = a.partition('t')#rpartition
+#b = a.rpartition('free')#cắt từ phải
+print(b)
+'''
 #tìm số lượng chữ e từ vị trí 0 đến vị trí 10 
 b = a.count('e',0,10)
 b = a.startswith('How',4,9)#vị trí 0 đến vị trí 10
 #b = a.endwith() 
-b = a.find('Kteam')#tìm vị trí có rfind,còn tìm k thấy sẽ quăng ra -1
+b = a.find('Kteam')#tìm vị trí có find,còn tìm k thấy sẽ quăng ra -1
 #b = a.index('y')# nếu tìm k thấy sẽ quăng ra lỗi 
 b = a.islower()#isupper
 b = a.isdigit()# kiểm tra có phải là số không 
@@ -19,4 +25,4 @@ d = s.lstrip('aaaAAaaaooaa')
 k = d.rstrip('aaaaaaa')
 h = k.replace('O','o')
 q = h.title()# viết hoa các chữ cái đầu dòng
-print('aaaAAaaaooaaneu mot Ngay na0 Doaaaaaaa'.strip('aA').lstrip('ao').title().replace('0','o'))
+print('aaaAAaaaooaaneu mot Ngay na0 Doaaaaaaa'.strip('aA').lstrip('ao').title().replace('0','o'))'''
