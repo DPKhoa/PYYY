52c30596f33fb2d03f158ddf01e1f8bc0bbbc6b2
Khoa <anhk312001@gmail.com>
2021-09-26 02:01:05 +0700

    sosanh

---
+++ sosanh.py
@@ -0,0 +1,7 @@
+#a = id('Khoa')#địa chỉ của giá trị trong bộ nhớ
+#print(a)
+#trả về một số nguyên int hoặc longint và nó sẽ không thay đổi suốt CT
+n = 69
+print(n)
+print(n * 2)
+print(n.__mul__(2))
