a7e4daf221588c1790a17470859416d78e0db306
Khoa <anhk312001@gmail.com>
2021-10-03 01:28:00 +0700

    dic2.py

---
+++ dic2.py
@@ -0,0 +1,29 @@
+d = {'Team':'Kteam',(1,2):69}
+#print(d)
+'''d2 = d.copy()
+print(d2)
+value =d.get('Tds','Free education')# lấy ra value dic theo Key
+# nếu như k có sẽ ra giá trị default
+print(value)
+value = d.items()# ra 1 tuple  với giá trị key:value
+print(value)
+value = list(d.items())
+print(value[0][1])'''
+#value = d.keys()
+#print(value)
+#.pop dua key value ra neu k co se de gia tri default 
+#value = d.popitem()
+#print(value)
+#print(d)
+#tra ve gia tri key trong dic 
+'''value = d.setdefault('What the fuck??','hello')
+print(value)
+print(d)
+value =d .update(Team='Free education')
+print(d)'''
+d = {}
+d.update({'a': 3})
+print(d)
+d = {}
+d.update(3)
+print(d)
